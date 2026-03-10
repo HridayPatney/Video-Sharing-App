65721b532930bb850854ff8736879cb15cbeefc7
Hriday Patney <hriday.patney05@gmail.com>
2026-03-10 22:33:58 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@
 //require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 dotenv.config({path:'./env'})
-  
+import {app} from './app.js'
 import connectDb from './db/index.js'
 
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((err)=>{
+    console.log('Error connecting to database',err)
+
+}) 
 
 
 /*
